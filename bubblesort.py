0760a63587f21ebf5cf7921a8745cf2325179fd9
vishnukumarkvs <116954249+vishnukumarkvs@users.noreply.github.com>
2022-11-05 21:38:22 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -12,3 +12,7 @@ print('Final answer:',bubblesort(c))
 
 #repeatedly comparing pairs of adjacent elements and swapping them
 #as a result, sorting goeas from right to left
+
+#Complexity
+#correct one = n(n-1)/2 == n*n == (1st cycle: n-1 comparisions, 2nd cycle: n-2 comparisions ...  last cycle: 1 comparision)
+#Best case: O(n) [if array is already sorted]
